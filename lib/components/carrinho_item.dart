e73a4db78801f0843aaab3c6ecd4d182fea63e87
@jian.licio <@jian.licio>
2022-06-09 17:26:34 -0300

    excluindo  itens do carrinho

--- lib/components/carrinho_item.dart
+++ lib/components/carrinho_item.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:loja/models/item_carrinho.dart';
+import 'package:provider/provider.dart';
+
+import '../models/carrinho.dart';
 
 class CarrinhoItemWidget extends StatelessWidget {
   final ItemCarrinho carrinhoItem;
@@ -9,21 +12,42 @@ class CarrinhoItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: ListTile(
-        leading: CircleAvatar(
-          child: Padding(
-            padding: const EdgeInsets.all(3),
-            child: FittedBox(
-              child: Text('${carrinhoItem.preco}'),
+    return Dismissible(
+      key: ValueKey(carrinhoItem.id),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 30,
+        ),
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 10),
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      ),
+      onDismissed: (_) {
+        Provider.of<Carrinho>(context, listen: false)
+            .removeItem(carrinhoItem.produtoId);
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: Expanded(
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: const EdgeInsets.all(3),
+                child: FittedBox(
+                  child: Text('${carrinhoItem.preco}'),
+                ),
+              ),
             ),
+            title: Text(carrinhoItem.nome),
+            subtitle: Text(
+                'total:R\$ ${carrinhoItem.preco * carrinhoItem.quantidade}'),
+            trailing: Text('${carrinhoItem.quantidade}x'),
           ),
         ),
-        title: Text(carrinhoItem.nome),
-        subtitle:
-            Text('total:R\$ ${carrinhoItem.preco * carrinhoItem.quantidade}'),
-        trailing: Text('${carrinhoItem.quantidade}x'),
       ),
     );
   }
