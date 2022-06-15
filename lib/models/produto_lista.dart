8b21ba9bc8e9408dadd154340205606401f37243
@jian.licio <@jian.licio>
2022-06-15 17:24:15 -0300

    iniciando com a firebase

--- lib/models/produto_lista.dart
+++ lib/models/produto_lista.dart
@@ -1,11 +1,14 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 import 'package:loja/data/dummy_data.dart';
 import 'package:loja/models/produto.dart';
 
 class ProdutoLista with ChangeNotifier {
   final List<Produto> _itens = dummyProdutos;
+  final _baseUrl = 'https://loja-jian-default-rtdb.firebaseio.com';
 
   List<Produto> get itens => [..._itens];
   List<Produto> get itensFavoritos =>
@@ -15,7 +18,7 @@ class ProdutoLista with ChangeNotifier {
     return _itens.length;
   }
 
-  void saveProduct(Map<String, Object> data) {
+  Future<void> saveProduct(Map<String, Object> data) {
     bool hasId = data['id'] != null;
 
     final produto = Produto(
@@ -27,24 +30,48 @@ class ProdutoLista with ChangeNotifier {
     );
 
     if (hasId) {
-      atualizarProduto(produto);
+      return atualizarProduto(produto);
     } else {
-      addProduto(produto);
+      return addProduto(produto);
     }
   }
 
-  void addProduto(Produto produto) {
-    _itens.add(produto);
+  Future<void> addProduto(Produto produto) async {
+    final response = await http.post(
+      Uri.parse('$_baseUrl/produtos.json'),
+      body: jsonEncode({
+        "nome": produto.nome,
+        "descricao": produto.descricao,
+        "preco": produto.preco,
+        "imagemUrl": produto.imagemUrl,
+        "isFavorite": produto.isFavorite,
+      }),
+    );
+
+    final id = jsonDecode(response.body)['name'];
+    print(id);
+    _itens.add(
+      Produto(
+        id: id,
+        nome: produto.nome,
+        descricao: produto.descricao,
+        preco: produto.preco,
+        imagemUrl: produto.imagemUrl,
+        isFavorite: produto.isFavorite,
+      ),
+    );
     notifyListeners();
   }
 
-  void atualizarProduto(Produto produto) {
+  Future<void> atualizarProduto(Produto produto) {
     int index = _itens.indexWhere((p) => p.id == produto.id);
 
     if (index >= 0) {
       _itens[index] = produto;
       notifyListeners();
     }
+
+    return Future.value();
   }
 
   void removeProduct(Produto produto) {
--- lib/pages/produto_form_pagina.dart
+++ lib/pages/produto_form_pagina.dart
@@ -26,6 +26,8 @@ class _ProdutoFormPaginaState extends State<ProdutoFormPagina> {
   final _formKey = GlobalKey<FormState>();
   final _formData = <String, Object>{};
 
+  bool _estaCarregando = false;
+
   @override
   void initState() {
     super.initState();
@@ -74,7 +76,7 @@ class _ProdutoFormPaginaState extends State<ProdutoFormPagina> {
     return isValidUrl && endsWithFile;
   }
 
-  void _submitForm() {
+  Future<void> _submitForm() async {
     final isValid = _formKey.currentState?.validate() ?? false;
 
     if (!isValid) {
@@ -83,12 +85,37 @@ class _ProdutoFormPaginaState extends State<ProdutoFormPagina> {
 
     _formKey.currentState?.save();
 
-    Provider.of<ProdutoLista>(
-      context,
-      listen: false,
-    ).saveProduct(_formData);
+    setState(() {
+      _estaCarregando = true;
+    });
 
-    Navigator.of(context).pop();
+    try {
+      await Provider.of<ProdutoLista>(
+        context,
+        listen: false,
+      ).saveProduct(_formData);
+      Navigator.of(context).pop();
+    } catch (erro) {
+      return await showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text(
+            'Ocorreu um erro!',
+          ),
+          content: const Text(
+              'Ocorreu um erro ao salvar o produto.\nContate o suporte'),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Ok'))
+          ],
+        ),
+      );
+    } finally {
+      setState(() => _estaCarregando = false);
+    }
   }
 
   @override
@@ -103,133 +130,136 @@ class _ProdutoFormPaginaState extends State<ProdutoFormPagina> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: [
-              TextFormField(
-                initialValue: _formData['nome']?.toString(),
-                decoration: const InputDecoration(labelText: 'Nome'),
-                textInputAction: TextInputAction.next,
-                onFieldSubmitted: (_) {
-                  FocusScope.of(context).requestFocus(_precoFocus);
-                },
-                onSaved: (nome) => _formData['nome'] = nome ?? '',
-                validator: (_name) {
-                  final name = _name ?? '';
+      body: _estaCarregando
+          ? const Center(child: CircularProgressIndicator())
+          : Padding(
+              padding: const EdgeInsets.all(15),
+              child: Form(
+                key: _formKey,
+                child: ListView(
+                  children: [
+                    TextFormField(
+                      initialValue: _formData['nome']?.toString(),
+                      decoration: const InputDecoration(labelText: 'Nome'),
+                      textInputAction: TextInputAction.next,
+                      onFieldSubmitted: (_) {
+                        FocusScope.of(context).requestFocus(_precoFocus);
+                      },
+                      onSaved: (nome) => _formData['nome'] = nome ?? '',
+                      validator: (_name) {
+                        final name = _name ?? '';
 
-                  if (name.trim().isEmpty) {
-                    return 'Nome é obrigatório';
-                  }
+                        if (name.trim().isEmpty) {
+                          return 'Nome é obrigatório';
+                        }
 
-                  if (name.trim().length < 4) {
-                    return 'Nome precisa no mínimo de 4 letras.';
-                  }
-
-                  return null;
-                },
-              ),
-              TextFormField(
-                initialValue: _formData['preco']?.toString(),
-                decoration: const InputDecoration(labelText: 'Preço'),
-                textInputAction: TextInputAction.next,
-                focusNode: _precoFocus,
-                keyboardType: const TextInputType.numberWithOptions(
-                  decimal: true,
-                ),
-                onFieldSubmitted: (_) {
-                  FocusScope.of(context).requestFocus(_descricaoFocus);
-                },
-                onSaved: (preco) =>
-                    _formData['preco'] = double.parse(preco ?? '0'),
-                validator: (_preco) {
-                  final precoString = _preco ?? '';
-                  final preco = double.tryParse(precoString) ?? -1;
-
-                  if (preco <= 0) {
-                    return 'Informe um preço válido.';
-                  }
-
-                  return null;
-                },
-              ),
-              TextFormField(
-                initialValue: _formData['descricao']?.toString(),
-                decoration: const InputDecoration(labelText: 'Descrição'),
-                textInputAction: TextInputAction.next,
-                focusNode: _descricaoFocus,
-                keyboardType: TextInputType.multiline,
-                maxLines: 3,
-                onSaved: (descricao) =>
-                    _formData['descricao'] = descricao ?? '',
-                validator: (_descricao) {
-                  final descricao = _descricao ?? '';
-
-                  if (descricao.trim().isEmpty) {
-                    return 'Descrição é obrigatória.';
-                  }
-
-                  if (descricao.trim().length < 5) {
-                    return 'Descrição precisa no mínimo de 5 letras.';
-                  }
-
-                  return null;
-                },
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      decoration:
-                          const InputDecoration(labelText: 'Url da Imagem'),
-                      keyboardType: TextInputType.url,
-                      textInputAction: TextInputAction.done,
-                      focusNode: _imageUrlFocus,
-                      controller: _imageUrlController,
-                      onFieldSubmitted: (_) => _submitForm(),
-                      onSaved: (imagemUrl) =>
-                          _formData['imagemUrl'] = imagemUrl ?? '',
-                      validator: (_imagemUrl) {
-                        final imageUrl = _imagemUrl ?? '';
-
-                        if (!isValidImageUrl(imageUrl)) {
-                          return 'Informe uma Url válida!';
+                        if (name.trim().length < 4) {
+                          return 'Nome precisa no mínimo de 4 letras.';
                         }
 
                         return null;
                       },
                     ),
-                  ),
-                  Container(
-                    height: 100,
-                    width: 100,
-                    margin: const EdgeInsets.only(
-                      top: 10,
-                      left: 10,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey,
-                        width: 1,
+                    TextFormField(
+                      initialValue: _formData['preco']?.toString(),
+                      decoration: const InputDecoration(labelText: 'Preço'),
+                      textInputAction: TextInputAction.next,
+                      focusNode: _precoFocus,
+                      keyboardType: const TextInputType.numberWithOptions(
+                        decimal: true,
                       ),
+                      onFieldSubmitted: (_) {
+                        FocusScope.of(context).requestFocus(_descricaoFocus);
+                      },
+                      onSaved: (preco) =>
+                          _formData['preco'] = double.parse(preco ?? '0'),
+                      validator: (_preco) {
+                        final precoString = _preco ?? '';
+                        final preco = double.tryParse(precoString) ?? -1;
+
+                        if (preco <= 0) {
+                          return 'Informe um preço válido.';
+                        }
+
+                        return null;
+                      },
                     ),
-                    alignment: Alignment.center,
-                    child: _imageUrlController.text.isEmpty
-                        ? const Text('Informe a Url')
-                        : FittedBox(
-                            child: Image.network(_imageUrlController.text),
-                            fit: BoxFit.cover,
+                    TextFormField(
+                      initialValue: _formData['descricao']?.toString(),
+                      decoration: const InputDecoration(labelText: 'Descrição'),
+                      textInputAction: TextInputAction.next,
+                      focusNode: _descricaoFocus,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 3,
+                      onSaved: (descricao) =>
+                          _formData['descricao'] = descricao ?? '',
+                      validator: (_descricao) {
+                        final descricao = _descricao ?? '';
+
+                        if (descricao.trim().isEmpty) {
+                          return 'Descrição é obrigatória.';
+                        }
+
+                        if (descricao.trim().length < 5) {
+                          return 'Descrição precisa no mínimo de 5 letras.';
+                        }
+
+                        return null;
+                      },
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                                labelText: 'Url da Imagem'),
+                            keyboardType: TextInputType.url,
+                            textInputAction: TextInputAction.done,
+                            focusNode: _imageUrlFocus,
+                            controller: _imageUrlController,
+                            onFieldSubmitted: (_) => _submitForm(),
+                            onSaved: (imagemUrl) =>
+                                _formData['imagemUrl'] = imagemUrl ?? '',
+                            validator: (_imagemUrl) {
+                              final imageUrl = _imagemUrl ?? '';
+
+                              if (!isValidImageUrl(imageUrl)) {
+                                return 'Informe uma Url válida!';
+                              }
+
+                              return null;
+                            },
                           ),
-                  ),
-                ],
+                        ),
+                        Container(
+                          height: 100,
+                          width: 100,
+                          margin: const EdgeInsets.only(
+                            top: 10,
+                            left: 10,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.grey,
+                              width: 1,
+                            ),
+                          ),
+                          alignment: Alignment.center,
+                          child: _imageUrlController.text.isEmpty
+                              ? const Text('Informe a Url')
+                              : FittedBox(
+                                  child:
+                                      Image.network(_imageUrlController.text),
+                                  fit: BoxFit.cover,
+                                ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
